3f832d5a83fdc5a7fcdd2b9ae5d81557fb5631b5
Chap Ambrose <chapambrose@gmail.com>
2019-12-09 16:45:42 +0000

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
 
+get '/hi' do
+  "hi"
+end
+
 get '/' do
-  "Greetings!"
+  ua = Net::HTTP.new('https://sinatra-greetings.herokuapp.com/hi', 443)
+    ua.instance_eval {
+      @ssl_context = OpenSSL::SSL::SSLContext.new
+#       options = OpenSSL::SSL::OP_NO_SSLv2 | OpenSSL::SSL::OP_NO_SSLv3
+#       if OpenSSL::SSL.const_defined?('OP_NO_COMPRESSION')
+#         options |= OpenSSL::SSL::OP_NO_COMPRESSION
+#       end
+#       @ssl_context.set_params({options: options})
+      @ssl_context.ssl_version = :TLSv1_3
+    }
+   # ua.instance_eval { @ssl_context = OpenSSL::SSL::SSLContext.new(:TLSv1_2) }
+    ua.use_ssl = true
+  ua.request 
 end
 
 #
